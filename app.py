be626a90519a89ea8fc19a338c6836be0710dd59
Nyko77771 <nykytamcdonald@gmail.com>
2025-11-24 17:02:18 +0000

    'First Commit'

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+# Creating flask instance
+app = Flask(__name__)
+ren = render_template
+
+# Creating a route decorator for home
+@app.route('/')
+def index():
+    return ren('login.html')
+
+@app.route('/user/<name>')
+def userPage(name):
+    return "<h1>Hello {}</h1>".format(name)
+
+
+if __name__ == "__main__":
+    app.run()
